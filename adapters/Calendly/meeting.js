c431da52517c87b10bf1f0ea7420e3fe4f588794
Derek Cavaliero <derek@derekcavaliero.com>
2022-09-18 11:21:29 -0700

    Calendly/meetings.js - v1.0.0

---
+++ adapters/Calendly/meeting.js
@@ -0,0 +1,87 @@
+/**
+ * GTM dataLayer Adapter for Calendly embedded meeting interactions.
+ * Version: 1.0.0
+ * Github: https://github.com/derekcavaliero/gtm-adapters
+ * Copyright (c) 2022 Derek Cavaliero <@derekcavaliero>
+ * Credits: 
+ * References: 
+ */
+
+dataLayer = window.dataLayer || [];
+window.addEventListener('message', function(message) {
+
+    var allowedOrigins = [
+        'https://calendly.com'
+    ];
+
+    if (allowedOrigins.indexOf(message.origin) === -1) 
+        return;
+
+    var action;
+
+    /**
+     * `message.data.event` is an assumed value and may not exist. 
+     * This may need set to a different property in the `message.data` object.
+     */
+    var messageType = message.data.event.replace('calendly.', ''); 
+
+    /**
+     * Message : Action mapping
+     * 
+     * The resulting action will be prefixed with the singular object name (e.g. `meeting_`)
+     * 
+     * This object determines the following:
+     * - Which messages are being transformed into dataLayer events.
+     * - What the resulting action
+     * 
+     * If you wish to "unsubscribe" or ignore particular message types, simply omit them 
+     * or comment out the key: value pair.
+     */
+    var subscribeTo = {
+        profile_page_viewed: 'list_view',
+        event_type_viewed: 'type_view',
+        date_and_time_selected: 'slot_selected',
+        event_scheduled: 'booked'
+    };
+
+    if (!subscribeTo.hasOwnProperty(messageType)) 
+        return;
+
+    action = subscribeTo[messageType];
+
+    var iframe = document.querySelectorAll('iframe[src^=' + message.origin + ']');
+
+    var meetingUrl = iframe.length == 1 ? iframe.src : undefined;
+    var meetingParams = new URLSearchParams(meetingUrl);
+
+    var platform = 'calendly',
+        namespace = platform,
+        object = 'meeting';
+
+    var eventPayload = {
+        
+        /**
+         * Should result in the following dataLayer events:
+         * 
+         * - hubspot.form_loaded
+         * - hubspot.form_failed_validation
+         * - hubspot.form_submit
+         * - hubspot.form_submitted
+         */
+        event: namespace + '.' + object + '_' + action,
+
+        event_context: {
+            platform: platform,
+            object: object,
+            meeting_url: meetingUrl.split('?')[0],
+            embed_type: meetingParams.get('embed_type')
+        },
+
+        user_context: {}
+
+    };
+
+    if (action)
+        window.dataLayer.push(eventPayload);
+
+});
